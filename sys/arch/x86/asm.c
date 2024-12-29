2084f3f1aa63026155ca01bca9d3a7810d37839a
gimura2022 <gimura0001@gmail.com>
2024-12-30 00:53:29 +0300

    fixed asm.c

--- sys/arch/x86/asm.c
+++ sys/arch/x86/asm.c
@@ -2,28 +2,6 @@
 
 #include <arch/asm.h>
 
-#define get_reg(func, reg, type) \
-	type func(void) \
-	{ \
-		type value; \
-		asm volatile( \
-			"mov %%##reg, %0" \
-			:: \
-			"i" (value) \
-		); \
-		return value; \
-	}
-
-#define set_reg(func, reg, type) \
-	void func(type value) \
-	{ \
-		asm volatile( \
-			"mov %0, %%##reg" \
-			:: \
-			"i" (value) \
-		); \
-	}
-
 void outb(uint16_t port, uint8_t value)
 {
 	asm volatile(
@@ -44,8 +22,46 @@ void outw(uint16_t port, uint16_t value)
 	);
 }
 
-set_reg(set_cr0, cr0, uint32_t);
-set_reg(set_cr3, cr3, uint32_t);
+void set_cr0(uint32_t value)
+{
+	asm volatile(
+		"mov %0, %%cr0"
+		::
+		"a" (value)
+	);
+}
 
-get_reg(get_cr0, cr0, uint32_t)
-get_reg(get_cr3, cr3, uint32_t)
+void set_cr3(uint32_t value)
+{
+	asm volatile(
+		"mov %0, %%cr3"
+		::
+		"a" (value)
+	);
+}
+
+uint32_t get_cr0(void)
+{
+	uint32_t value;
+
+	asm volatile(
+		"mov %%cr0, %0"
+		:
+		"=r" (value)
+	);
+
+	return value;
+}
+
+uint32_t get_cr3(void)
+{
+	uint32_t value;
+
+	asm volatile(
+		"mov %%cr3, %0"
+		:
+		"=r" (value)
+	);
+
+	return value;
+}
