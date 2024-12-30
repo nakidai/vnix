4e5af2bf5b0c21e8ee83034b304c54adbc81028b
gimura2022 <gimura0001@gmail.com>
2024-12-30 20:34:52 +0300

    added heap initialisation

--- sys/main.c
+++ sys/main.c
@@ -3,8 +3,14 @@
 #include <vnix/interrput.h>
 #include <vnix/vmm.h>
 #include <vnix/mem_table.h>
+#include <vnix/kio.h>
+#include <vnix/heap.h>
 
 void kernel_entry(struct multiboot* args)
 {
+	kputs("Initing heap...");
+	heap_init(vmm_get_page_by_addr(args->mem_upper * 1024) - 1);
+	kok();
+
 	halt();
 }
